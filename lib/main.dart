ee5b440dc2c0e91c37677b241447aa8fa9f6c20f
sohagwu <144034712+sohagwu@users.noreply.github.com>
2026-09-01 14:02:39 +0600

    Create main.dart

---
+++ lib/main.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Trading Signal App',
+      theme: ThemeData.dark(),
+      home: const TradingScreen(),
+    );
+  }
+}
+
+class TradingScreen extends StatefulWidget {
+  const TradingScreen({super.key});
+
+  @override
+  State<TradingScreen> createState() => _TradingScreenState();
+}
+
+class _TradingScreenState extends State<TradingScreen> {
+  late final WebViewController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = WebViewController()
+      ..setJavaScriptMode(JavaScriptMode.unrestricted)
+      ..loadFlutterAsset('assets/index.html');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Trading Chart'),
+        centerTitle: true,
+      ),
+      body: WebViewWidget(controller: _controller),
+    );
+  }
+}
